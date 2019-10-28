2b01db841ec7eee5af1e20794539513cb4e623c8
rianoktiomersaputra <rianoktiomersaputra@gmail.com>
2019-10-28 22:28:13 +0700

    Create database

---
+++ isnan.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 28, 2019 at 04:27 PM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `isnan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ph`
+--
+
+CREATE TABLE `ph` (
+  `id` int(11) NOT NULL,
+  `waktu` date NOT NULL,
+  `ph` float NOT NULL,
+  `suhu` float NOT NULL,
+  `tingkatgaram` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ph`
+--
+
+INSERT INTO `ph` (`id`, `waktu`, `ph`, `suhu`, `tingkatgaram`) VALUES
+(1, '2019-10-28', 7, 20.1, 2),
+(2, '2019-10-27', 7.2, 20.8, 2.3),
+(3, '2019-10-29', 6.8, 18.9, 1.8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(2, 'isnan', '6a0e9e2fc662576a9398259e75fd3a2ece885f1b');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ph`
+--
+ALTER TABLE `ph`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ph`
+--
+ALTER TABLE `ph`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
