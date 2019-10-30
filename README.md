de809e21e0f0141f8b19877d5c011b024ee7baf3
rianoktiomersaputra <rianoktiomersaputra@gmail.com>
2019-10-31 01:31:10 +0700

    Change database and readme file

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # freelance-kolamPemancingan
 
-Kalo seandaikan gagal ngeload, 
-Ubah file di folder config/config.php 
+- Konfigurasi gagal ngeload, 
+    - Ubah $base_url di file config.php folder config (config/config.php)
+        dari $base_url = "localhost/freelance/kolamPemancingan" 
+            -> $base_url = "localhost/kolamPemancingan" kalo langsung naronya di folder htdocs 
+            -> $base_url = "localhost/NamaFolder/kolamPemancingan" kalo nyimpannya di folder lain (NamaFolder disesuaikan)
 
-$base_url = "localhost/freelance/kolamPemancingan" diganti jadi lokasi nyimpan folder kolamPemancingan di htdocsnya :)
+- Update (31/10/2019) :
+    1. (+) Email penerima sudah sesuai dengan kolom email, dari tabel user, database kolamPemancingan
+    2. (+) Grafik sudah menampilkan data dari tanggal 14 hari dari sekarang sampai sekarang
+    3. (-) Pas loading di dashboard, lambat karena harus ngirim email ke seluruh user yang terdaftar (Aku bikin 3 user tadi, vendor emailnya ngasih warning "too_much_take_respond", gimana kalo 10 user ato lebih ?) 
+    4, (?) Saran aku sesuaiin dulu maunya email yang nerima itu seluruh user ato user yang login aja ?
+
--- kolampemancingan.sql
+++ kolampemancingan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 30, 2019 at 03:35 PM
+-- Generation Time: Oct 30, 2019 at 07:29 PM
 -- Server version: 10.3.15-MariaDB
 -- PHP Version: 7.3.6
 
@@ -43,7 +43,18 @@ CREATE TABLE `record` (
 INSERT INTO `record` (`id`, `waktu`, `ph`, `suhu`, `tingkatgaram`) VALUES
 (1, '2019-10-28', 7, 20.1, 2),
 (2, '2019-10-27', 7.2, 20.8, 2.3),
-(3, '2019-10-29', 6.8, 18.9, 1.8);
+(3, '2019-10-29', 6.8, 18.9, 1.8),
+(4, '2019-10-26', 7.3, 21, 6),
+(5, '2019-10-25', 7.4, 22, 4),
+(6, '2019-10-24', 0, 0, 0),
+(7, '2019-10-23', 0, 0, 0),
+(8, '2019-10-22', 7.9, 22.3, 6.6),
+(9, '2019-10-21', 7.4, 20.5, 2.9),
+(10, '2019-10-20', 6, 15, 7),
+(11, '2019-10-19', 8, 25, 3),
+(12, '2019-10-18', 6.6, 17, 8),
+(13, '2019-10-17', 6.5, 17, 3),
+(14, '2019-10-16', 6, 11, 7);
 
 -- --------------------------------------------------------
 
@@ -53,6 +64,7 @@ INSERT INTO `record` (`id`, `waktu`, `ph`, `suhu`, `tingkatgaram`) VALUES
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
+  `email` varchar(256) NOT NULL,
   `username` varchar(128) NOT NULL,
   `password` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -61,8 +73,8 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(2, 'isnan', '6a0e9e2fc662576a9398259e75fd3a2ece885f1b');
+INSERT INTO `user` (`id`, `email`, `username`, `password`) VALUES
+(2, 'isnaenim05@gmail.com', 'isnan', '6a0e9e2fc662576a9398259e75fd3a2ece885f1b');
 
 --
 -- Indexes for dumped tables
@@ -88,13 +100,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `record`
 --
 ALTER TABLE `record`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
